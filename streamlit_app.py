aae4a1e0f1844f9c85e3a8456ae19e4828d9857f
nickjohnson2005 <umair.naeem89@gmail.com>
2023-06-16 18:13:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,12 @@ except URLError as e:
       streamlit.error()
 
 
-#output it on the screen as a table
-streamlit.dataframe(fruityvice_normalized)
-
-
+#New Section to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!"
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+     back_from_function = get_fruityvice_data(fruit_choice)
+     streamlit.dataframe(back_from_function)
