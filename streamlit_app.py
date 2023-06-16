e80785d67354a26c9605a08c831de838f2d8f41f
nickjohnson2005 <umair.naeem89@gmail.com>
2023-06-16 18:50:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,11 +14,10 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-
 def_get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
-  my_cur.execute("select * from fruit_load_list")
-  return my_cur.fetchall()
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
 
 #Add a button to load the fruit
   if streamlit.button('Gret Fruit Load List'):
